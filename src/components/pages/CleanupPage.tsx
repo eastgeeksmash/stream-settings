bf8d6c16d9fa192431d2a88a8a021ae642374c19
Shugo Kawamura <ruk1ium@gmail.com>
2025-05-14 07:30:15 +0900

    Buttonの状態管理を変更

--- src/components/pages/CleanupPage.tsx
+++ src/components/pages/CleanupPage.tsx
@@ -2,69 +2,80 @@ import type React from 'react';
 import { Button } from '../ui/button';
 import { invoke } from '@tauri-apps/api/core';
 import { toast } from 'sonner';
+import { useState } from 'react';
 
 export const CleanupPage: React.FC = () => {
+  const [isDiscordLogoutLoading, setIsDiscordLogoutLoading] = useState(false);
+  const [isChromeLogoutLoading, setIsChromeLogoutLoading] = useState(false);
+  const [isDownloadDirectoryDeleteLoading, setIsDownloadDirectoryDeleteLoading] = useState(false);
+
+  const logoutDiscord = () => {
+    setIsDiscordLogoutLoading(true);
+    invoke('logout_discord')
+      .then(() => {
+        toast.success('Discordのログアウトが完了しました');
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        toast.error(`Discordのログアウトに失敗しました: ${error.message}`);
+      })
+      .finally(() => {
+        setIsDiscordLogoutLoading(false);
+      });
+  }
+
+  const logoutChrome = () => {
+    setIsChromeLogoutLoading(true);
+    invoke('logout_chrome')
+      .then(() => {
+        toast.success('Chromeのログアウトが完了しました');
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        toast.error(`Chromeのログアウトに失敗しました: ${error.message}`);
+      })
+      .finally(() => {
+        setIsChromeLogoutLoading(false);
+      });
+  }
+
+  const deleteDownloadDirectory = () => {
+    setIsDownloadDirectoryDeleteLoading(true);
+    invoke('delete_download_directory')
+      .then(() => {
+        toast.success('ダウンロードディレクトリの削除が完了しました');
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        toast.error(`ダウンロードディレクトリの削除に失敗しました: ${error.message}`);
+      })
+      .finally(() => {
+        setIsDownloadDirectoryDeleteLoading(false);
+      });
+  }
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4">Cleanup</h2>
       
       <ul className="space-y-3">
         <li>
-          <Button type="button" className="w-full" variant="default" onClick={(event) => {
-            const button = event.currentTarget as HTMLButtonElement;
-            button.disabled = true;
-
-            invoke('logout_discord')
-              .then(() => {
-                toast.success('Discordのログアウトが完了しました');
-              })
-              .catch((error: Error) => {
-                console.error(error);
-                toast.error(`Discordのログアウトに失敗しました: ${error.message}`);
-              })
-              .finally(() => {
-                button.disabled = false;
-              });
+          <Button type="button" className="w-full" variant="default" disabled={isDiscordLogoutLoading} onClick={(event) => {
+            logoutDiscord();
           }}>
             Discordログアウト
           </Button>
         </li>
         <li>
-          <Button type="button" className="w-full" variant="default" onClick={(event) => {
-            const button = event.currentTarget as HTMLButtonElement;
-            button.disabled = true;
-
-            invoke('logout_chrome')
-              .then(() => {
-                toast.success('Chromeのログアウトが完了しました');
-              })
-              .catch((error: Error) => {
-                console.error(error);
-                toast.error(`Chromeのログアウトに失敗しました: ${error.message}`);
-              })
-              .finally(() => {
-                button.disabled = false;
-              });
+          <Button type="button" className="w-full" variant="default" disabled={isChromeLogoutLoading} onClick={(event) => {
+            logoutChrome();
           }}>
             Chromeログアウト
           </Button>
         </li>
         <li>
-          <Button type="button" className="w-full" variant="default" onClick={(event) => {
-            const button = event.currentTarget as HTMLButtonElement;
-            button.disabled = true;
-
-            invoke('delete_download_directory')
-              .then(() => {
-                toast.success('ダウンロードディレクトリの削除が完了しました');
-              })
-              .catch((error: Error) => {
-                console.error(error);
-                toast.error(`ダウンロードディレクトリの削除に失敗しました: ${error.message}`);
-              })
-              .finally(() => {
-                button.disabled = false;
-              });
+          <Button type="button" className="w-full" variant="default" disabled={isDownloadDirectoryDeleteLoading} onClick={(event) => {
+            deleteDownloadDirectory();
           }}>
             ダウンロードディレクトリを削除
           </Button>
--- src/components/pages/SetupPage.tsx
+++ src/components/pages/SetupPage.tsx
@@ -2,29 +2,50 @@ import type React from 'react';
 import { Button } from '../ui/button';
 import { toast } from "sonner"
 import { invoke } from '@tauri-apps/api/core';
+import { useState } from 'react';
 
 
 export const SetupPage: React.FC = () => {
+  const [isMakeNetworkPrivateLoading, setIsMakeNetworkPrivateLoading] = useState(false);
+  const [isChangePowerSettingsLoading, setIsChangePowerSettingsLoading] = useState(false);
+
+  const makeNetworkPrivate = () => {
+    setIsMakeNetworkPrivateLoading(true);
+    invoke('make_network_private')
+      .then(() => {
+        toast.success("ネットワークのプライベート化が完了しました");
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        toast.error(`ネットワークのプライベート化に失敗しました: ${error.message}`);
+      })
+      .finally(() => {
+        setIsMakeNetworkPrivateLoading(false);
+      });
+  }
+
+  const changePowerSettings = () => {
+    setIsChangePowerSettingsLoading(true);
+    invoke('change_power_settings')
+      .then(() => {
+        toast.success("電源設定の変更が完了しました");
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        toast.error(`電源設定の変更に失敗しました: ${error.message}`);
+      })
+      .finally(() => {
+        setIsChangePowerSettingsLoading(false);
+      });
+  }
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4">Setup</h2>
       <ul className="space-y-3">
         <li>
-          <Button type="button" className="w-full" variant="default" onClick={(event) => {
-            const button = event.currentTarget as HTMLButtonElement;
-            button.disabled = true;
-            
-            invoke('make_network_private')
-              .then(() => {
-                toast.success("ネットワークのプライベート化が完了しました");
-              })
-              .catch((error: Error) => {
-                console.error(error);
-                toast.error(`ネットワークのプライベート化に失敗しました: ${error.message}`);
-              })
-              .finally(() => {
-                button.disabled = false;
-              });
+          <Button type="button" className="w-full" variant="default" disabled={isMakeNetworkPrivateLoading} onClick={() => {
+            makeNetworkPrivate();
           }}>
             ネットワークをプライベート化
           </Button>
@@ -40,15 +61,9 @@ export const SetupPage: React.FC = () => {
             className="w-full"
             variant="default"
             title="高パフォーマンスモードに変更、スリープモード無効化、電源ボタンのロックを行います"
+            disabled={isChangePowerSettingsLoading}
             onClick={() => {
-              invoke('change_power_settings')
-                .then(() => {
-                  toast.success("電源設定の変更が完了しました");
-                })
-                .catch((error: Error) => {
-                  console.error(error);
-                  toast.error(`電源設定の変更に失敗しました: ${error.message}`);
-                });
+              changePowerSettings();
             }}
           >
             電源設定の変更
